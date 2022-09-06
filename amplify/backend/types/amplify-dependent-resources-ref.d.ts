945e4f1b43e72fc9d0236277974097c8fe5f4635
lathspell37 <awnasf@hotmail.com>
2022-09-06 09:36:35 +0300

    frontend done

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "tiktok": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
